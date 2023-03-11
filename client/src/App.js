aa578cb8f21734f9a2c1a886a378eeeb32121827
AAKASH GORADIA <80081102+AakashGoradia@users.noreply.github.com>
2023-03-11 16:31:22 +0530

    landing done

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Landing from './pages/Landing';
 const App=()=> {
   return (
     <>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Landing/>
     </>
   );
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -27,14 +27,14 @@ const Landing=()=> {
 </header> */}
 {/* {/* <section class="text-gray-700 body-font bg-[#ff4500]"> */}
 <section class="text-gray-700 body-font bg-black">
-  <div class="container mx-auto flex px-5 lg:py-8 lg:px-4 md:flex-row flex-col items-center">
+  <div class="container mx-auto flex px-5 lg:py-20 lg:px-4 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center lg:ml-4">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-bold text-white font-lato">Welcome to DigiLocker
 
       </h1>
       <p class="mb-8 leading-relaxed font-medium font-lato text-white">Forget the hassle of carrying your important documents everywhere. We have made verification and other services online for you. Sign Up Now and get started.</p>
       <div class="flex justify-center">
-        <button class="inline-flex text-black bg-white py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg border-black border-2 font-medium font-lato">Sign Up</button>
+        <button class="inline-flex text-black bg-white py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg border-black border-2 font-semibold font-lato">Sign Up/Log In</button>
         <button class="border-black border-2 font-medium ml-4 inline-flex text-white font-lato bg-black py-2 px-6 focus:outline-none underline rounded text-lg">Go to Dashboard</button>
       </div>
     </div>
@@ -45,7 +45,7 @@ const Landing=()=> {
   </div>
 </section>
 <section class="text-gray-700 body-font border-t border-gray-200">
-  <div class="container px-5 py-24 mx-auto">
+  <div class="container px-5 py-16 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h2 class="text-xs text-black tracking-widest font-semibold title-font mb-1 font-lato">Get To Know Us</h2>
       <h1 class="sm:text-3xl text-2xl font-bold title-font text-gray-900 font-lato">OUR SERVICES</h1>
@@ -177,8 +177,21 @@ const Landing=()=> {
     </div>
   </div>
 </section> */}
+<section class="text-gray-700 body-font border-t border-gray-200 bg-black">
+  <div class="container px-5 py-8 mx-auto">
+    <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
+        <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+      </svg>
+      <p class="leading-relaxed text-lg font-semibold font-lato text-white"> We want to free our citizens from the burden of excessive paper documents in every office. We want paperless transactions. We will set up a digital locker for every citizen to store personal documents that can be shared across departments.</p>
+      <span class="inline-block h-1 w-10 rounded bg-gray-400 mt-8 mb-6"></span>
+      <h2 class="text-white font-bold font-lato title-font tracking-wider text-sm">NARENDRA MODI</h2>
+      <p class="text-white font-medium font-lato">Prime Minister Of India</p>
+    </div>
+  </div>
+</section>
 <section class="text-gray-700 body-font border-t border-gray-200">
-  <div class="container px-5 py-24 mx-auto">
+  <div class="container px-5 py-16 mx-auto">
     <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
       <h1 class="sm:text-3xl text-2xl title-font mb-2 text-gray-900 font-lato font-bold">DigiLocker Documents</h1>
       <p class="lg:w-1/2 w-full leading-relaxed text-base font-semibold">Find and Manage your different type of Documents here.</p>
@@ -257,7 +270,7 @@ const Landing=()=> {
     {/* <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button> */}
   </div>
 </section>
-<section class="text-gray-700 body-font border-t border-gray-200">
+{/* <section class="text-gray-700 body-font border-t border-gray-200">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
@@ -347,8 +360,8 @@ const Landing=()=> {
       </div>
     </div>
   </div>
-</section>
-<section class="text-gray-700 body-font overflow-hidden border-t border-gray-200">
+</section> */}
+{/* <section class="text-gray-700 body-font overflow-hidden border-t border-gray-200">
   <div class="container px-5 py-24 mx-auto flex flex-wrap">
     <div class="lg:w-1/4 mt-48 hidden lg:block">
       <div class="mt-px border-t border-gray-300 border-b border-l rounded-tl-lg rounded-bl-lg overflow-hidden">
@@ -547,36 +560,23 @@ const Landing=()=> {
       </div>
     </div>
   </div>
-</section>
-<section class="text-gray-700 body-font border-t border-gray-200">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
-        <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-      </svg>
-      <p class="leading-relaxed text-lg">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware. Man bun next level coloring book skateboard four loko knausgaard. Kitsch keffiyeh master cleanse direct trade indigo juice before they sold out gentrify plaid gastropub normcore XOXO 90's pickled cindigo jean shorts. Slow-carb next level shoindigoitch ethical authentic, yr scenester sriracha forage franzen organic drinking vinegar.</p>
-      <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-      <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
-      <p class="text-gray-500">Senior Product Designer</p>
-    </div>
-  </div>
-</section>
+</section> */}
+
 <section class="text-gray-700 body-font relative">
-  <div class="absolute inset-0 bg-gray-300">
-    {/* <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe> */}
+  <div class="absolute inset-0 bg-black">
   </div>
-  <div class="container px-5 py-24 mx-auto flex">
+  <div class="container px-5 py-8 mx-auto flex">
     <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-      <input class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" placeholder="Email" type="email"/>
-      <textarea class="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none" placeholder="Message"></textarea>
-      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+      <h2 class="text-gray-900 text-lg mb-1 font-semibold title-font font-lato">Write to Us</h2>
+      <p class="leading-relaxed mb-5 text-gray-600 font-medium font-lato">Feel free to reach out to us with anything.</p>
+      <input class="bg-white rounded border border-gray-400 focus:outline-none text-base px-4 py-2 mb-4" placeholder="Email" type="email"/>
+      <textarea class="bg-white rounded border border-gray-400 focus:outline-none h-32 text-base px-4 py-2 mb-4 resize-none" placeholder="Message"></textarea>
+      <button class="inline-flex text-white bg-black py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg border-black border-2 font-medium font-lato">Submit</button>
+      <p class="text-xs text-gray-500 mt-3">Your message will be only shared with us and not anybody else.</p>
     </div>
   </div>
 </section>
-<footer class="text-gray-700 body-font">
+{/* <footer class="text-gray-700 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap md:text-left text-center -mb-10 -mx-4">
       <div class="lg:w-1/6 md:w-1/2 w-full px-4">
@@ -726,7 +726,7 @@ const Landing=()=> {
       <span class="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Enamel pin tousled raclette tacos irony</span>
     </div>
   </div>
-</footer>
+</footer> */}
 <a href="https://github.com/AakashGoradia/LOC-Sorteddd" class="rounded-full w-12 h-12 bg-gray-100 fixed bottom-0 right-0 flex items-center justify-center text-gray-800 mr-8 mb-8 shadow-sm border-gray-300 border"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
   </svg></a>
