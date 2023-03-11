b2c434e4a22974dd33a876c656cc29249916a031
Smit Shah <101961910+smit-shah-GG@users.noreply.github.com>
2023-03-11 15:23:08 +0530

    update to orctest

--- Server/ocrtest.py
+++ Server/ocrtest.py
@@ -1,12 +1,31 @@
-import cv2 as cv
+import cv2
 import pytesseract
+import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv.imread('6284efbfffc3d519e87ad616_positive-quotes-1.jpg')
+import urllib.request
+
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
+
+opener = AppURLopener()
+response = opener.open('http://httpbin.org/user-agent')
+
+from urllib.request import urlopen
+def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
+    # download the image, convert it to a NumPy array, and then read
+    # it into OpenCV format
+    resp = opener.open(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, readFlag)
+
+    # return the image
+    return image
+
+img = url_to_image('https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/6284efbfffc3d519e87ad616_positive-quotes-1.jpg')
 
 roi = img
 
 alpha = pytesseract.image_to_string(roi)
 print(alpha)
-
